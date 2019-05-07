be72d5a4e24ca5adbef1e52bc3a6ded10b4741bb
leishendaohuo <49307448+leishendaohuo@users.noreply.github.com>
2019-05-07 20:29:36 +0800

    Update settings.py

--- Aproject/Aproject/settings.py
+++ Aproject/Aproject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test',
         'USER':'root',
-        'PASSWORD':'357159',
+        'PASSWORD':'123456',
         'HOST':'localhost',
         'PORT':'3306'
     }
